4afbe9783b3ad440267b0e81887e5f90b2fdcd0b
noahclanman <78673578+noahclanman@users.noreply.github.com>
2022-05-22 20:38:05 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# apt install iptables-persistent
 
-    iptables-save > iptables.bkp
+    iptables-save > /etc/iptables/rules.v4.backup
     iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5400
     iptables -t nat -I PREROUTING -p tcp --dport 8080 -j REDIRECT --to-ports 5500
     iptables -t nat -I PREROUTING -p tcp --dport 8880 -j REDIRECT --to-ports 5500
@@ -9,3 +10,4 @@
     iptables -t nat -I PREROUTING -p tcp --dport 2086 -j REDIRECT --to-ports 5400
     iptables -t nat -I PREROUTING -p tcp --dport 2095 -j REDIRECT --to-ports 5400
     iptables -t nat -I PREROUTING -p tcp --dport 8280 -j REDIRECT --to-ports 5400
+    iptables-save > /etc/iptables/rules.v4
