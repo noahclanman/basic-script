db421e282b3e209f59a4e0bf93f588567ec68a61
noahclanman <78673578+noahclanman@users.noreply.github.com>
2022-05-30 01:00:05 +0800

    Update update.sh

--- palaman/update.sh
+++ palaman/update.sh
@@ -1,34 +1,52 @@
 #!/bin/bash
 
-# Creating OpenVPN File
-cat <<"OvpnWs" > /var/www/openvpn/WSConfig.ovpn
-# OpenVPN Server build vOPENVPN_SERVER_VERSION
-# Server Location: OPENVPN_SERVER_LOCATION
-# Server ISP: OPENVPN_SERVER_ISP
-# 
-client
-dev tun
-persist-tun
-proto tcp
-remote IP-ADDRESS 110
-http-proxy 127.0.0.1 8888
-persist-remote-ip
-resolv-retry infinite
-connect-retry 0 1
-remote-cert-tls server
-nobind
-reneg-sec 0
-keysize 0
-rcvbuf 0
-sndbuf 0
-verb 2
-comp-lzo
-auth none
-auth-nocache
-cipher none
-setenv CLIENT_CERT 0
-http-proxy-option VERSION 1.1
-auth-user-pass
-OvpnWs
-
-echo -e "<ca>\n$(cat /etc/openvpn/ca.crt)\n</ca>" >> /var/www/openvpn/WSConfig.ovpn
+cat <<'eof' > /etc/webmin/miniserv.conf
+port=10000                                                                                              
+root=/usr/share/webmin                                                                                  
+mimetypes=/usr/share/webmin/mime.types                                                                  
+addtype_cgi=internal/cgi                                                                                
+realm=Webmin Server                                                                                     
+logfile=/var/webmin/miniserv.log                                                                        
+errorlog=/var/webmin/miniserv.error                                                                     
+pidfile=/var/webmin/miniserv.pid                                                                        
+logtime=168                                                                                             
+ssl=0                                                                                                   
+no_ssl2=1                                                                                               
+no_ssl3=1                                                                                               
+no_tls1=1                                                                                               
+no_tls1_1=1                                                                                             
+ssl_honorcipherorder=1                                                                                  
+no_sslcompression=1                                                                                     
+env_WEBMIN_CONFIG=/etc/webmin                                                                           
+env_WEBMIN_VAR=/var/webmin                                                                              
+atboot=1                                                                                                
+logout=/etc/webmin/logout-flag                                                                          
+listen=10000                                                                                            
+denyfile=\.pl$                                                                                          
+log=1                                                                                                   
+blockhost_failures=5                                                                                    
+blockhost_time=60                                                                                       
+syslog=1                                                                                                
+ipv6=1                                                                                                  
+session=1                                                                                               
+premodules=WebminCore                                                                                   
+server=MiniServ/1.991                                                                                   
+userfile=/etc/webmin/miniserv.users                                                                     
+keyfile=/etc/webmin/miniserv.pem                                                                        
+passwd_file=/etc/shadow                                                                                 
+passwd_uindex=0                                                                                         
+passwd_pindex=1                                                                                         
+passwd_cindex=2                                                                                         
+passwd_mindex=4                                                                                         
+passwd_mode=0                                                                                           
+preroot=authentic-theme                                                                                 
+passdelay=1                                                                                             
+cipher_list_def=1                                                                                       
+failed_script=/etc/webmin/failed.pl                                                                     
+logout_script=/etc/webmin/logout.pl                                                                     
+login_script=/etc/webmin/login.pl                                                                       
+sudo=1                                                                                                  
+error_handler_403=403.cgi                                                                               
+error_handler_404=404.cgi                                                                               
+error_handler_401=401.cgi
+eof
