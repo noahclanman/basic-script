4f35567a299ff3195dbdcc143117338c12f641f8
noahclanman <78673578+noahclanman@users.noreply.github.com>
2022-06-28 03:25:05 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -3,22 +3,22 @@
 
 iptables-save > /etc/iptables/rules.v4.backup
 
-iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
-iptables -I INPUT -p tcp --dport 8880 -j ACCEPT
+#iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+#iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
+#iptables -I INPUT -p tcp --dport 8880 -j ACCEPT
 iptables -I INPUT -p tcp --dport 2052 -j ACCEPT
 iptables -I INPUT -p tcp --dport 2082 -j ACCEPT
 iptables -I INPUT -p tcp --dport 2086 -j ACCEPT
 iptables -I INPUT -p tcp --dport 2095 -j ACCEPT
-iptables -I INPUT -p tcp --dport 8280 -j ACCEPT
+#iptables -I INPUT -p tcp --dport 8280 -j ACCEPT
 
-iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5400
-iptables -t nat -I PREROUTING -p tcp --dport 8080 -j REDIRECT --to-ports 5500
-iptables -t nat -I PREROUTING -p tcp --dport 8880 -j REDIRECT --to-ports 5500
-iptables -t nat -I PREROUTING -p tcp --dport 2052 -j REDIRECT --to-ports 5400
-iptables -t nat -I PREROUTING -p tcp --dport 2082 -j REDIRECT --to-ports 5400
-iptables -t nat -I PREROUTING -p tcp --dport 2086 -j REDIRECT --to-ports 5400
-iptables -t nat -I PREROUTING -p tcp --dport 2095 -j REDIRECT --to-ports 5400
-iptables -t nat -I PREROUTING -p tcp --dport 8280 -j REDIRECT --to-ports 5400
+#iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 0
+#iptables -t nat -I PREROUTING -p tcp --dport 8080 -j REDIRECT --to-ports 0
+#iptables -t nat -I PREROUTING -p tcp --dport 8880 -j REDIRECT --to-ports 0
+iptables -t nat -I PREROUTING -p tcp --dport 2052 -j REDIRECT --to-ports 80
+iptables -t nat -I PREROUTING -p tcp --dport 2082 -j REDIRECT --to-ports 80
+iptables -t nat -I PREROUTING -p tcp --dport 2086 -j REDIRECT --to-ports 80
+iptables -t nat -I PREROUTING -p tcp --dport 2095 -j REDIRECT --to-ports 80
+#iptables -t nat -I PREROUTING -p tcp --dport 8280 -j REDIRECT --to-ports 0
 
 iptables-save > /etc/iptables/rules.v4
