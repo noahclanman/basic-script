aa75d9194d88ff6b91e5c852b99897603182028a
noahclanman <78673578+noahclanman@users.noreply.github.com>
2023-02-19 03:41:58 +0800

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 if [[ "$(command -v squid)" ]]; then
- if [[ "$(squid -v | grep -Ec '(V|v)ersion\s3.5.23')" -lt 1 ]]; then
+ if [[ "$(squid -v | grep -Ec '(V|v)ersion\s4.6')" -lt 1 ]]; then
   apt remove --purge squid -y -f 2>/dev/null
-  wget "http://security.debian.org/debian-security/pool/updates/main/s/squid3/squid_3.5.23-5+deb9u5_amd64.deb" -qO squid.deb
+  wget "http://security.debian.org/debian-security/pool/updates/main/s/squid3/squid3_4.6-1+deb10u8_all.deb" -qO squid.deb
   dpkg -i squid.deb
   rm -f squid.deb
  else
-  echo -e "Squid v3.5.23 already installed"
+  echo -e "Squid v4.6 already installed"
  fi
 else
- if [[ "$(cat /etc/os-release | grep -E 'VERSION_ID=\"9|10|11\"' -c)" -gt 0 ]]; then
-  apt install libecap3 squid-common squid-langpack -y -f 2>/dev/null
-  wget "http://security.debian.org/debian-security/pool/updates/main/s/squid3/squid_3.5.23-5+deb9u5_amd64.deb" -qO squid.deb
-  dpkg -i squid.deb
-  rm -f squid.deb
- else
-  echo -e "Unsupported Debian version"
- fi
+ apt install libecap3 squid-common squid-langpack -y -f 2>/dev/null
+ wget "http://security.debian.org/debian-security/pool/updates/main/s/squid3/squid3_4.6-1+deb10u8_all.deb" -qO squid.deb
+ dpkg -i squid.deb
+ rm -f squid.deb
 fi
 
     
